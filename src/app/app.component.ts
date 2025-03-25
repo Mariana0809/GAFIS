388d86ee06b6068f4667acc991858731f1ec7d19
Mariana Cervantes <marianac080923@gmail.com>
2025-03-25 11:47:58 -0500

    Inicio del diseño del login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginInstructorComponent } from './componets/login-instructor/login-instructor.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginInstructorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/componets/login-instructor/login-instructor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-instructor',
+  imports: [],
+  templateUrl: './login-instructor.component.html',
+  styleUrl: './login-instructor.component.css'
+})
+export class LoginInstructorComponent {
+
+}
