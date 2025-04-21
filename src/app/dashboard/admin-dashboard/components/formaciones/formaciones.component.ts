2863a01310e8b99f7d6f1fe27b4741b05f91a146
Mariana Cervantes <marianac080923@gmail.com>
2025-04-21 11:54:07 -0500

    Inicio del calendario para el dashboard

---
+++ src/app/dashboard/admin-dashboard/components/formaciones/formaciones.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formaciones',
+  imports: [],
+  templateUrl: './formaciones.component.html',
+  styleUrl: './formaciones.component.css'
+})
+export class FormacionesComponent {
+
+}
---
+++ src/app/dashboard/admin-dashboard/components/instructores/instructores.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instructores',
+  imports: [],
+  templateUrl: './instructores.component.html',
+  styleUrl: './instructores.component.css'
+})
+export class InstructoresComponent {
+
+}
